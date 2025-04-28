4d2fd9ecfaf5ca45b8ba6cc3e61ce334754617bd
Esteban Zenteno Espinoza <spowoky.uwu@gmail.com>
2025-04-28 20:42:14 +0000

    Añadido de Libro y Libro repository

---
+++ src/main/java/com/example/bibliotecaduoc/model/Libro.java
@@ -0,0 +1,17 @@
+package com.example.bibliotecaduoc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Libro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private String editorial;
+    private String anioPublicacion;
+    private String isbn;
+}
