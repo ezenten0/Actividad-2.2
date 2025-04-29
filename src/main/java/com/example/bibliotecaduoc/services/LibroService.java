691615a040e8d0917237c9f89e13971dd426c50a
Esteban Zenteno Espinoza <spowoky.uwu@gmail.com>
2025-04-29 00:37:34 +0000

    Agregado y modificado: LibroController y LibroService
    - Se agregó LibroController para manejar las solicitudes relacionadas con "Libro".
    - Se agregó LibroService para gestionar la lógica de negocio de "Libro".

---
+++ src/main/java/com/example/bibliotecaduoc/services/LibroService.java
@@ -0,0 +1,40 @@
+package com.example.bibliotecaduoc.services;
+
+import com.example.bibliotecaduoc.model.Libro;
+import com.example.bibliotecaduoc.repository.LibroRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LibroService {
+    @Autowired
+    private LibroRepository libroRepository;
+
+    public List<Libro> getLibros() {
+        return libroRepository.obtenerLibros();
+    }
+
+    public Libro saveLibro(Libro lib) {
+        return libroRepository.guardar(lib);
+    }
+
+    public Libro getLibroId(int id) {
+        return libroRepository.buscarPorId(id);
+    }
+
+    public Libro uptdateLibro(Libro lib) {
+        return libroRepository.actualizar(lib);
+    }
+
+    public String deleteLibro(int id) {
+        libroRepository.eliminar(id);
+        return "Libro eliminado";
+    }
+
+    public Libro getLibroIsbn(String isbn) {
+        return libroRepository.buscarPorIsbn(isbn);
+    }
+}
