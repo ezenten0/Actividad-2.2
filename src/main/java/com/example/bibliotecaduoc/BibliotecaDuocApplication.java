b2c4fc871c464c915171e5ff031222a89dc5864c
Esteban Zenteno Espinoza <spowoky.uwu@gmail.com>
2025-04-29 01:31:49 +0000

    Creación de la clase BibliotecaDuocApplication y HomeController, Elminiación de la clase DemoApplicationTests

---
+++ src/main/java/com/example/bibliotecaduoc/BibliotecaDuocApplication.java
@@ -0,0 +1,12 @@
+package com.example.bibliotecaduoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDuocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaDuocApplication.class, args);
+    }
+}
