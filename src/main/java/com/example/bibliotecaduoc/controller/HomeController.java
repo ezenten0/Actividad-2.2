b6378a49d036ba7a52beb92df562307b00571da6
Esteban Zenteno Espinoza <spowoky.uwu@gmail.com>
2025-04-29 01:44:10 +0000

    Eliminación de la clase HomeController.java Cambios en el pom.xml para cambiar la ruta principal de la aplicación

--- src/main/java/com/example/bibliotecaduoc/controller/HomeController.java
+++ src/main/java/com/example/bibliotecaduoc/controller/HomeController.java
@@ -1,13 +1,13 @@
-package com.example.bibliotecaduoc.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HomeController {
-
-    @GetMapping("/")
-    public String home() {
-        return "Bienvenido a la Biblioteca Duoc!";
-    }
-}
+// package com.example.bibliotecaduoc.controller;
+//
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
+//
+// @RestController
+// public class HomeController {
+//
+// @GetMapping("/")
+// public String home() {
+// return "Bienvenido a la Biblioteca Duoc!";
+// }
+// }
